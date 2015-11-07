c4d100f6ff43e84246847594c04d198599d3f130
Sean Dion <smdion@gmail.com>
2015-11-07 16:18:35 -0600

    run git pull as abc

--- init/30_git_update.sh
+++ init/30_git_update.sh
@@ -4,5 +4,5 @@ if [ ! -d "/app/htpcmanager/.git" ]; then
 /sbin/setuser abc git clone https://github.com/Hellowlol/HTPC-Manager.git /app/htpcmanager
 else
 cd /app/htpcmanager
-git pull
+/sbin/setuser abc git pull
 fi
